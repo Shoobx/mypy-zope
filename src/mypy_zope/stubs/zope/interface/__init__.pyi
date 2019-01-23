04ae6b9f1a28525632aa3b146e46f140bf788cf4
Andrey Lebedev <andrey@lebedev.lt>
2019-01-23 15:08:19 -0500

    Reexport more names fro zope.interface

--- src/mypy_zope/stubs/zope/interface/__init__.pyi
+++ src/mypy_zope/stubs/zope/interface/__init__.pyi
@@ -4,6 +4,7 @@
 
 from zope.interface.declarations import Declaration as Declaration, alsoProvides as alsoProvides, classImplements as classImplements, classImplementsOnly as classImplementsOnly, classProvides as classProvides, directlyProvidedBy as directlyProvidedBy, directlyProvides as directlyProvides, implementedBy as implementedBy, implementer as implementer, implementer_only as implementer_only, implements as implements, implementsOnly as implementsOnly, moduleProvides as moduleProvides, noLongerProvides as noLongerProvides, providedBy as providedBy, provider as provider, named as named
 from zope.interface.interface import Attribute as Attribute, Interface as Interface
+from zope.exceptions import Invalid as Invalid, DoesNotImplement as DoesNotImplement, BrokenImplementation as BrokenImplementation, InvalidInterface as InvalidInterface, BadImplements as BadImplements
 from zope.interface import adapter as adapter
 from zope.interface import advice as advice
 from zope.interface import interfaces as interfaces
