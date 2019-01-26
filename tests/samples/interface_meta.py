4d5d839637cfc0d73023f7b26e889690ca58ae8c
Andrey Lebedev <andrey@lebedev.lt>
2019-01-26 17:18:34 -0500

    Make sure IInterface.providedBy works

---
+++ tests/samples/interface_meta.py
@@ -0,0 +1,25 @@
+import zope.interface
+from typing import Type, TypeVar
+
+T = TypeVar('T', bound='zope.interface.Interface')
+
+
+class IBookmark(zope.interface.Interface):
+    @staticmethod
+    def create(url: str) -> 'IBookmark':
+        pass
+
+def createOb(iface: Type[T]) -> T:
+    if zope.interface.interfaces.IInterface.providedBy(iface):
+        pass
+    return iface(None)
+
+def main(self) -> None:
+    bm = createOb(IBookmark)
+    reveal_type(bm)
+
+"""
+<output>
+interface_meta.py:19: error: Revealed type is '__main__.IBookmark*'
+</output>
+"""
