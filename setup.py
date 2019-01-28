85c0418e7c7b87c5d289b88fc6ba5ed577d47877
Andrey Lebedev <andrey@lebedev.lt>
2019-01-27 23:09:16 -0500

    PEP-8

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'mypy-zope',
-    version = '0.1',
-    author = 'Andrey Lebedev',
-    author_email = 'andrey.lebedev@gmail.com',
-    description = 'Plugin for mypy to support zope interfaces',
+    name='mypy-zope',
+    version='0.1',
+    author='Andrey Lebedev',
+    author_email='andrey.lebedev@gmail.com',
+    description='Plugin for mypy to support zope interfaces',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     install_requires = [
