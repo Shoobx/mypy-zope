a9f539035d0122378c76de1720dfbece89081cc8
Andrey Lebedev <andrey@lebedev.lt>
2019-03-05 21:57:11 +0200

    Add long description for pypi

--- setup.py
+++ setup.py
@@ -1,18 +1,27 @@
 from setuptools import setup, find_packages
 
+long_description = """
+# Plugin for mypy to support zope.interface
+
+The goal is to be able to make zope interfaces to be treated as types in mypy
+sense.
+
+For feature description and usage instructions, see https://github.com/Shoobx
+/mypy-zope.
+"""
+
 setup(
     name="mypy-zope",
     version="0.1.2",
     author="Andrey Lebedev",
     author_email="andrey.lebedev@gmail.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     description="Plugin for mypy to support zope interfaces",
     packages=find_packages("src"),
     package_dir={"": "src"},
     install_requires=["mypy>=0.660", "zope.interface", "zope.schema"],
     extras_require={"test": ["pytest>=4.0.0,<4.1.0", "pytest-cov", "lxml"]},
-    dependency_links=[
-        "git+https://github.com/python/mypy.git@release-0.660#egg=mypy-0.660"
-    ],
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Environment :: Console",
