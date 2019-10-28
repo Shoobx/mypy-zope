5866219d1b112b3487ee884f982a5d911b00981a
Andrey Lebedev <andrey@lebedev.lt>
2019-10-28 20:09:07 +0200

    Back to development: 0.2.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,12 @@
 
 All notable changes to this project will be documented in this file.
 
+0.2.3 (unreleased)
+------------------
+
+- Nothing changed yet.
+
+
 0.2.2 (2019-10-28)
 ------------------
 
--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ For feature description and usage instructions, see
 
 setup(
     name="mypy-zope",
-    version="0.2.2",
+    version="0.2.3.dev0",
     author="Andrey Lebedev",
     author_email="andrey.lebedev@gmail.com",
     long_description=long_description,
