eb4b48be2fe19fa760a0f17d33fe426c039e76c8
arnimarj <arnimarj@gmail.com>
2019-11-30 16:42:34 +0000

    fixing a few things, including the tests

--- tests/samples/interface_unknown_direct.py
+++ tests/samples/interface_unknown_direct.py
@@ -17,7 +17,7 @@ def main() -> None:
 
 """
 <output>
-interface_unknown_direct.py:7: error: Cannot find module named 'unknown.interfaces'
+interface_unknown_direct.py:7: error: Cannot find implementation or library stub for module named 'unknown.interfaces'
 interface_unknown_direct.py:7: note: See https://mypy.readthedocs.io/en/latest/running_mypy.html#missing-imports
 interface_unknown_direct.py:15: note: Revealed type is 'Any'
 </output>
