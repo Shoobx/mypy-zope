a7893c1f2810ded77c10bb52802259a5fb983222
Roel Bruggink <roel@jaroel.nl>
2019-12-30 01:24:40 +0100

    Update mypy version pin

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     description="Plugin for mypy to support zope interfaces",
     packages=["mypy_zope", "zope-stubs"],
     package_dir={"": "src"},
-    install_requires=["mypy==0.750", "zope.interface", "zope.schema"],
+    install_requires=["mypy==0.761", "zope.interface", "zope.schema"],
     extras_require={"test": ["pytest", "pytest-cov", "lxml"]},
     classifiers=[
         "Development Status :: 3 - Alpha",
