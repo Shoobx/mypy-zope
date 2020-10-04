a5a353369c69300b89d5cf2c4773e7974dca5a36
Jason R. Coombs <jaraco@jaraco.com>
2020-10-04 11:06:49 -0400

    Rely on fullname to more precisely detect an InterfaceClass instance.

--- src/mypy_zope/plugin.py
+++ src/mypy_zope/plugin.py
@@ -259,7 +259,11 @@ class ZopeInterfacePlugin(Plugin):
     ) -> Optional[Callable[[ClassDefContext], None]]:
         # print(f"get_metaclass_hook: {fullname}")
         def analyze_metaclass(ctx: ClassDefContext) -> Callable[[ClassDefContext], None]:
-            if ctx.cls.metaclass.name == 'InterfaceClass':
+            def is_zope_interface_class(info):
+                expected = 'zope.interface.interface.InterfaceClass'
+                return info and any(node.fullname == expected for node in info.mro)
+
+            if is_zope_interface_class(ctx.cls.metaclass.node):
                 md = self._get_metadata(ctx.cls.info)
                 md["is_interface"] = True
         return analyze_metaclass
