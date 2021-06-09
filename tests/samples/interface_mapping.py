0db84ab01e076a63d7d3c825b3fb77c61381e080
Arni Mar Jonsson <arnimarj@gmail.com>
2021-06-09 12:54:23 +0000

    wip

--- tests/samples/interface_mapping.py
+++ tests/samples/interface_mapping.py
@@ -24,8 +24,8 @@ if __name__ == '__main__':
 """
 <output>
 interface_mapping.py:8: error: 'BadContainer' is missing following 'zope.interface.common.mapping.IItemMapping' interface members: __getitem__.
-interface_mapping.py:8: error: 'BadContainer' is missing following 'zope.interface.common.mapping.IReadMapping' interface members: get, __contains__.
-interface_mapping.py:8: error: 'BadContainer' is missing following 'zope.interface.common.mapping.IEnumerableMapping' interface members: keys, __iter__, items, __len__.
+interface_mapping.py:8: error: 'BadContainer' is missing following 'zope.interface.common.mapping.IReadMapping' interface members: __contains__, get.
+interface_mapping.py:8: error: 'BadContainer' is missing following 'zope.interface.common.mapping.IEnumerableMapping' interface members: __iter__, __len__, items, keys.
 interface_mapping.py:8: error: 'BadContainer' is missing following 'zope.interface.common.mapping.IWriteMapping' interface members: __delitem__, __setitem__.
 </output>
 """
