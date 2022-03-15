8df6bdb4147efde2682a2bbf47871ef100c2dabb
Andrey Lebedev <andrey@lebedev.lt>
2022-03-15 09:09:59 +0200

    Upgrade to mypy-0.941
    The type of open() has changed to real classes instead
    of abstract types. Adjust tests.

--- tests/samples/open.py
+++ tests/samples/open.py
@@ -1,9 +1,9 @@
 reveal_type(open('str'))
-reveal_type(open('str', 'b'))
+reveal_type(open('str', 'rb'))
 
 """
 <output>
-open.py:1: note: Revealed type is "typing.TextIO"
-open.py:2: note: Revealed type is "typing.BinaryIO"
+open.py:1: note: Revealed type is "io.TextIOWrapper"
+open.py:2: note: Revealed type is "io.BufferedReader"
 </output>
 """
